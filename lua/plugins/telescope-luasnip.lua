06af5de434d3d54b5ae2b26e0d247c6c9f7bc639
Ben Du <longendu@yahoo.com>
2026-05-02 01:13:25 -0700

    enable and config telescope-luasnip.lua

---
+++ lua/plugins/telescope-luasnip.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "benfowler/telescope-luasnip.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function() require("telescope").load_extension "luasnip" end,
+    -- Add a keybinding to open it
+    keys = {
+      { "<leader>ss", "<cmd>Telescope luasnip<cr>", desc = "Find snippets" },
+    },
+  },
+}
